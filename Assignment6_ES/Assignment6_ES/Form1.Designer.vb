653447142747c0fd8d04e43574dacf7d60740b84
EmsCode82 <emsgithub@gmail.com>
2024-01-08 17:32:44 -0500

    first commit

---
+++ Assignment6_ES/Assignment6_ES/Form1.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBobsPretzels
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPretzelCheese = New System.Windows.Forms.TextBox()
+        Me.txtPretzelNoCheese = New System.Windows.Forms.TextBox()
+        Me.lblIndividualSaleTotal = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblTotalCheeseSold = New System.Windows.Forms.Label()
+        Me.lblTotalNoCheeseSold = New System.Windows.Forms.Label()
+        Me.lblTotalSales = New System.Windows.Forms.Label()
+        Me.lblThisSaleTotal = New System.Windows.Forms.Label()
+        Me.lblTotalCheese = New System.Windows.Forms.Label()
+        Me.lblTotalNoCheese = New System.Windows.Forms.Label()
+        Me.lblDailyTotal = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtPretzelCheese
+        '
+        Me.txtPretzelCheese.Location = New System.Drawing.Point(132, 74)
+        Me.txtPretzelCheese.Name = "txtPretzelCheese"
+        Me.txtPretzelCheese.Size = New System.Drawing.Size(87, 20)
+        Me.txtPretzelCheese.TabIndex = 0
+        '
+        'txtPretzelNoCheese
+        '
+        Me.txtPretzelNoCheese.Location = New System.Drawing.Point(132, 30)
+        Me.txtPretzelNoCheese.Name = "txtPretzelNoCheese"
+        Me.txtPretzelNoCheese.Size = New System.Drawing.Size(87, 20)
+        Me.txtPretzelNoCheese.TabIndex = 1
+        '
+        'lblIndividualSaleTotal
+        '
+        Me.lblIndividualSaleTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblIndividualSaleTotal.Location = New System.Drawing.Point(132, 117)
+        Me.lblIndividualSaleTotal.Name = "lblIndividualSaleTotal"
+        Me.lblIndividualSaleTotal.Size = New System.Drawing.Size(87, 22)
+        Me.lblIndividualSaleTotal.TabIndex = 3
+        Me.lblIndividualSaleTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(29, 425)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(67, 21)
+        Me.btnSubmit.TabIndex = 6
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(119, 425)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(67, 21)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(209, 425)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(67, 21)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 77)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Cheese Pretzels:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Plain Pretzels:"
+        '
+        'lblTotalCheeseSold
+        '
+        Me.lblTotalCheeseSold.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalCheeseSold.Location = New System.Drawing.Point(125, 77)
+        Me.lblTotalCheeseSold.Name = "lblTotalCheeseSold"
+        Me.lblTotalCheeseSold.Size = New System.Drawing.Size(94, 26)
+        Me.lblTotalCheeseSold.TabIndex = 11
+        Me.lblTotalCheeseSold.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTotalNoCheeseSold
+        '
+        Me.lblTotalNoCheeseSold.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalNoCheeseSold.Location = New System.Drawing.Point(125, 32)
+        Me.lblTotalNoCheeseSold.Name = "lblTotalNoCheeseSold"
+        Me.lblTotalNoCheeseSold.Size = New System.Drawing.Size(94, 26)
+        Me.lblTotalNoCheeseSold.TabIndex = 12
+        Me.lblTotalNoCheeseSold.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTotalSales
+        '
+        Me.lblTotalSales.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalSales.Location = New System.Drawing.Point(125, 122)
+        Me.lblTotalSales.Name = "lblTotalSales"
+        Me.lblTotalSales.Size = New System.Drawing.Size(94, 26)
+        Me.lblTotalSales.TabIndex = 13
+        Me.lblTotalSales.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblThisSaleTotal
+        '
+        Me.lblThisSaleTotal.AutoSize = True
+        Me.lblThisSaleTotal.Location = New System.Drawing.Point(11, 122)
+        Me.lblThisSaleTotal.Name = "lblThisSaleTotal"
+        Me.lblThisSaleTotal.Size = New System.Drawing.Size(56, 13)
+        Me.lblThisSaleTotal.TabIndex = 14
+        Me.lblThisSaleTotal.Text = "Final Total"
+        '
+        'lblTotalCheese
+        '
+        Me.lblTotalCheese.AutoSize = True
+        Me.lblTotalCheese.Location = New System.Drawing.Point(5, 84)
+        Me.lblTotalCheese.Name = "lblTotalCheese"
+        Me.lblTotalCheese.Size = New System.Drawing.Size(109, 13)
+        Me.lblTotalCheese.TabIndex = 15
+        Me.lblTotalCheese.Text = "Daily Cheese Pretzels"
+        '
+        'lblTotalNoCheese
+        '
+        Me.lblTotalNoCheese.AutoSize = True
+        Me.lblTotalNoCheese.Location = New System.Drawing.Point(6, 39)
+        Me.lblTotalNoCheese.Name = "lblTotalNoCheese"
+        Me.lblTotalNoCheese.Size = New System.Drawing.Size(56, 13)
+        Me.lblTotalNoCheese.TabIndex = 16
+        Me.lblTotalNoCheese.Text = "Daily Plain"
+        '
+        'lblDailyTotal
+        '
+        Me.lblDailyTotal.AutoSize = True
+        Me.lblDailyTotal.Location = New System.Drawing.Point(6, 129)
+        Me.lblDailyTotal.Name = "lblDailyTotal"
+        Me.lblDailyTotal.Size = New System.Drawing.Size(60, 13)
+        Me.lblDailyTotal.TabIndex = 17
+        Me.lblDailyTotal.Text = "Total Sales"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtPretzelCheese)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtPretzelNoCheese)
+        Me.GroupBox1.Controls.Add(Me.lblThisSaleTotal)
+        Me.GroupBox1.Controls.Add(Me.lblIndividualSaleTotal)
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(248, 169)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Customer Sale"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotalSales)
+        Me.GroupBox2.Controls.Add(Me.lblTotalCheese)
+        Me.GroupBox2.Controls.Add(Me.lblDailyTotal)
+        Me.GroupBox2.Controls.Add(Me.lblTotalCheeseSold)
+        Me.GroupBox2.Controls.Add(Me.lblTotalNoCheeseSold)
+        Me.GroupBox2.Controls.Add(Me.lblTotalNoCheese)
+        Me.GroupBox2.Location = New System.Drawing.Point(29, 238)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(248, 169)
+        Me.GroupBox2.TabIndex = 19
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Daily Totals"
+        '
+        'frmBobsPretzels
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 481)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "frmBobsPretzels"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bob's Pretzels"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtPretzelCheese As TextBox
+    Friend WithEvents txtPretzelNoCheese As TextBox
+    Friend WithEvents lblIndividualSaleTotal As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblTotalCheeseSold As Label
+    Friend WithEvents lblTotalNoCheeseSold As Label
+    Friend WithEvents lblTotalSales As Label
+    Friend WithEvents lblThisSaleTotal As Label
+    Friend WithEvents lblTotalCheese As Label
+    Friend WithEvents lblTotalNoCheese As Label
+    Friend WithEvents lblDailyTotal As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Assignment6_ES/Assignment6_ES/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class frmBobsPretzels
+    Dim intTotalCheeseCount As Integer
+    Dim intTotalNoCheeseCount As Integer
+    Dim decTotalSalesCount As Decimal
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        'Declare Variables
+        Dim intNumberPretzelCheese As Integer
+        Dim intNumberPretzelNoCheese As Integer
+        Dim decIndividualSaleTotal As Decimal
+
+
+        'Get Inputs
+        intNumberPretzelCheese = txtPretzelCheese.Text
+        intNumberPretzelNoCheese = txtPretzelNoCheese.Text
+
+        'Do Calculations
+        decIndividualSaleTotal = (intNumberPretzelCheese * 5) + (intNumberPretzelNoCheese * 2.5)
+
+        intTotalCheeseCount += intNumberPretzelCheese
+        intTotalNoCheeseCount += intNumberPretzelNoCheese
+        decTotalSalesCount += decIndividualSaleTotal
+
+
+        'Display Outputs
+        lblIndividualSaleTotal.Text = FormatCurrency(decIndividualSaleTotal)
+        lblTotalCheeseSold.Text = intTotalCheeseCount
+        lblTotalNoCheeseSold.Text = intTotalNoCheeseCount
+        lblTotalSales.Text = FormatCurrency(decTotalSalesCount)
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtPretzelCheese.Clear()
+        txtPretzelNoCheese.Clear()
+        lblIndividualSaleTotal.ResetText()
+        txtPretzelNoCheese.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        Close()
+
+    End Sub
+
+End Class
